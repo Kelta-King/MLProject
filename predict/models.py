3a5bb06d6e4c8de42f672c9fabef748a8ec182f3
Kushang Shah <kushangshah131@gmail.com>
2021-04-08 13:06:26 +0530

    models

--- predict/models.py
+++ predict/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Disease(models.Model):
+    diseaseName = models.CharField(unique = True, max_length = 500)
+    def __str__(self):
+        return self.diseaseName
+
+
+class Messages(models.Model):
+    messageText = models.TextField()
+    diseaseName = models.ForeignKey(Disease, on_delete = models.CASCADE)
