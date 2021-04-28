9bc9bc24b9569a452cdff5ae21debf257939c0dd
Kushang Shah <kushangshah131@gmail.com>
2021-04-28 09:04:28 +0530

    lr pkl

--- Models/logisticRegression.py
+++ Models/logisticRegression.py
@@ -1,6 +1,24 @@
+"""
+---- Later prediction code ----
+
+import pickle
+
+loaded_model = pickle.load(open('Pkls/lr_model.pkl', 'rb'))
+result = loaded_model.predict([[0, 0, 0, 1, 0, 0, 0, 0]])
+print(result)
+
+
+
+Model training code is below
+
+
+"""
+
+
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import pickle
 
 model = LogisticRegression()
 data = pd.read_excel('E:\ML Project\Project\MLProject\Dataset\covid_prediction_ds.xlsx')
@@ -9,6 +27,20 @@ le = LabelEncoder()
 data['test_indication_numeric'] = le.fit_transform(data.test_indication)
 data['gender_numeric'] = le.fit_transform(data.gender)
 data['age_60_and_above_numeric'] = le.fit_transform(data.age_60_and_above)
+
+X = data[['cough', 'fever', 'sore_throat', 'shortness_of_breath', 'head_ache', 'age_60_and_above_numeric', 'gender_numeric', 'test_indication_numeric']]
+
+data['predict'] = le.fit_transform(data.corona_result)
+y = data['predict']
+
+model.fit(X, y)
+
+filename = "lr_model.pkl"
+pickle.dump(model, open(filename, 'wb'))
+
+print(model.predict([[0, 0, 0, 0, 0, 0, 0, 0]]))
+
+
 
 """
 male 2
@@ -23,22 +55,7 @@ Abroad 0
 No 0
 Yes 2
 None 1
-"""
 
-
-X = data[['cough', 'fever', 'sore_throat', 'shortness_of_breath', 'head_ache', 'age_60_and_above_numeric', 'gender_numeric', 'test_indication_numeric']]
-#print(X)
-data['predict'] = le.fit_transform(data.corona_result)
-y = data['predict']
-#print(data[['predict', 'corona_result']])
-"""
 negative 0
 positive 1
 """
-
-#print(data['corona_result'][0])
-#print(data['predict'][0])
-#print(y)
-model.fit(X, y)
-
-print(model.predict([[0, 0, 0, 0, 0, 0, 0, 0]]))
